98bf71134709ffdb651587a53398166c3b2fc2fb
Conor Murphy <conmurph@cisco.com>
2021-10-20 14:34:53 +0200

    Updated IKS profile to use emear_pvt_infra

--- main.tf
+++ main.tf
@@ -4,67 +4,67 @@ provider "intersight" {
   endpoint  = var.intersight_endpoint
 }
 
-# module "terraform-intersight-iks" {
-#     source  = "terraform-cisco-modules/iks/intersight"
-#     #source = "app.terraform.io/Cisco-IST-TigerTeam/iks/intersight"
-#     version ="2.0.3"
+module "terraform-intersight-iks" {
+    source  = "terraform-cisco-modules/iks/intersight"
+    #source = "app.terraform.io/Cisco-IST-TigerTeam/iks/intersight"
+    version ="2.0.3"
 
-#     cluster = {
-#         name = var.iks_cluster_name
-#         action = "Deploy"
-#         wait_for_completion = false
-#         worker_nodes = 1
-#         load_balancers = 4
-#         worker_max = 20
-#         control_nodes = 1
-#         ssh_user = var.iks_ssh_user
-#         ssh_public_key = var.iks_ssh_key
-#     }
+    cluster = {
+        name = var.iks_cluster_name
+        action = "Deploy"
+        wait_for_completion = false
+        worker_nodes = 1
+        load_balancers = 4
+        worker_max = 20
+        control_nodes = 1
+        ssh_user = var.iks_ssh_user
+        ssh_public_key = var.iks_ssh_key
+    }
 
-#     ip_pool = {
-#         use_existing = true
-#         name = "RMLAB-IKS-IPPOOL-130"
-#     }
+    ip_pool = {
+        use_existing = true
+        name = "RMLAB-IKS-IPPOOL-130"
+    }
 
-#     sysconfig = {
-#         use_existing = true
-#         name = "rmlab-iks-sys-config-policy"
-#     }
+    sysconfig = {
+        use_existing = true
+        name = "rmlab-iks-sys-config-policy"
+    }
 
-#     k8s_network = {
-#         use_existing = true
-#         name = "rmlab-iks-network-policy"
-#     }
+    k8s_network = {
+        use_existing = true
+        name = "rmlab-iks-network-policy"
+    }
 
-#     runtime_policy = {
-#         use_existing = true
-#         create_new = false
-#         name = "evolve_iks_cluster_runtime_policy"
-#     }
+    runtime_policy = {
+        use_existing = true
+        create_new = false
+        name = "evolve_iks_cluster_runtime_policy"
+    }
 
-#     version_policy = {
-#         use_existing = true
-#         name = "RMLAB-IKS-K8S-VERSION"
-#     }
+    version_policy = {
+        use_existing = true
+        name = "RMLAB-IKS-K8S-VERSION"
+    }
     
     
-#     tr_policy = {
-#         use_existing = false
-#         create_new = false
-#     }
+    tr_policy = {
+        use_existing = false
+        create_new = false
+    }
     
 
-#     # Infra Config Policy Information
-#     infra_config_policy = {
-#         use_existing = true
-#         name = "emear_pvt_iks_vm_infra"
-#     }
+    # Infra Config Policy Information
+    infra_config_policy = {
+        use_existing = true
+        name = "emear_pvt_iks_vm_infra"
+    }
 
-#     instance_type = {
-#         use_existing = true
-#         name = "RMLAB-IKS-VM-INSTANCE-TYPE"
-#     }
+    instance_type = {
+        use_existing = true
+        name = "RMLAB-IKS-VM-INSTANCE-TYPE"
+    }
     
-#     organization = var.intersight_organization
-# }
+    organization = var.intersight_organization
+}
 
