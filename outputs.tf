b458df2047c1854323c3d7620df88782f06a3f0d
Conor Murphy <conmurph@cisco.com>
2021-10-20 16:50:54 +0200

    Adding output for kubeconfig

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "kube_config" {
-  value = module.terraform-intersight-iks.cluster_profile.kube_config[0].kube_config
+  value = module.terraform-intersight-iks.module.cluster_profile.kube_config[0].kube_config
   depends_on = [module.terraform-intersight-iks]
 }
